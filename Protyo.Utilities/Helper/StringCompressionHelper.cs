ff34f3a4d7e39c24ac309f1214c1e82b7075eb00
Rafat Khandaker <Rafat.Khandaker@gmail.com>
2024-04-23 04:07:29 -0400

    checkin StringCompressionHelper class for byte compression of Dynamo DB data storage limits

---
+++ Protyo.Utilities/Helper/StringCompressionHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protyo.Utilities.Helper
+{
+    public class StringCompressionHelper
+    {
+        public byte[] CompressString(string inputString)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(inputString);
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (GZipStream gzipStream = new GZipStream(memoryStream, CompressionMode.Compress, true))
+                {
+                    gzipStream.Write(bytes, 0, bytes.Length);
+                }
+
+                return memoryStream.ToArray();
+            }
+        }
+        public string DecompressBytes(byte[] compressedBytes)
+        {
+            using (MemoryStream memoryStream = new MemoryStream(compressedBytes))
+            {
+                using (GZipStream gzipStream = new GZipStream(memoryStream, CompressionMode.Decompress))
+                {
+                    using (StreamReader streamReader = new StreamReader(gzipStream))
+                    {
+                        return streamReader.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
